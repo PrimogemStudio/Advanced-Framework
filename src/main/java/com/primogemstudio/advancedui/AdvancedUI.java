230e824b584833eeccde9823e5066dd6a447a8a8
Jack253-png <guoxiuchen20170402@163.com>
2023-12-07 18:36:42 +0800

    提交项目基础

---
+++ src/main/java/com/primogemstudio/advancedui/AdvancedUI.java
@@ -0,0 +1,9 @@
+package com.primogemstudio.advancedui;
+
+import net.fabricmc.api.ModInitializer;
+
+public class AdvancedUI implements ModInitializer {
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/primogemstudio/advancedui/client/AdvancedUIClient.java
@@ -0,0 +1,9 @@
+package com.primogemstudio.advancedui.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AdvancedUIClient implements ClientModInitializer {
+    public void onInitializeClient() {
+
+    }
+}
