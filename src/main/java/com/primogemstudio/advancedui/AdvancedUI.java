28666b67eebb181244080490a02639d71aa340ef
Jack253-png <guoxiuchen20170402@163.com>
2023-12-11 20:57:23 +0800

    更改 Shader 初始化位置

--- src/main/java/com/primogemstudio/advancedui/AdvancedUI.java
+++ src/main/java/com/primogemstudio/advancedui/AdvancedUI.java
@@ -1,12 +1,10 @@
 package com.primogemstudio.advancedui;
 
-import com.primogemstudio.advancedui.render.Shaders;
 import net.fabricmc.api.ModInitializer;
 
 public class AdvancedUI implements ModInitializer {
     public static final String MOD_ID = "advancedui";
-
     public void onInitialize() {
-        Shaders.init();
+
     }
 }
--- src/main/java/com/primogemstudio/advancedui/client/AdvancedUIClient.java
+++ src/main/java/com/primogemstudio/advancedui/client/AdvancedUIClient.java
@@ -1,9 +1,10 @@
 package com.primogemstudio.advancedui.client;
 
+import com.primogemstudio.advancedui.render.Shaders;
 import net.fabricmc.api.ClientModInitializer;
 
 public class AdvancedUIClient implements ClientModInitializer {
     public void onInitializeClient() {
-
+        Shaders.init();
     }
 }
