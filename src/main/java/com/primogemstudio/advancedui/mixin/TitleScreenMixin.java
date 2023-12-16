d8c54e08a63604facc0f16a0060069b3b952536f
Jack253-png <guoxiuchen20170402@163.com>
2023-12-16 13:47:26 +0800

    暂时禁用 Shader 圆角

--- src/main/java/com/primogemstudio/advancedui/mixin/TitleScreenMixin.java
+++ src/main/java/com/primogemstudio/advancedui/mixin/TitleScreenMixin.java
@@ -3,7 +3,9 @@ package com.primogemstudio.advancedui.mixin;
 import com.primogemstudio.advancedui.render.FilterTypes;
 import com.primogemstudio.advancedui.render.RenderQueue;
 import com.primogemstudio.advancedui.render.shape.RoundedRectangle;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.TitleScreen;
 import org.joml.Vector2f;
 import org.joml.Vector4f;
@@ -12,13 +14,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(TitleScreen.class)
+@Mixin(Screen.class)
 public abstract class TitleScreenMixin {
     @Inject(at = @At("HEAD"), method = "render")
     public void renderWidget(GuiGraphics graphics, int mouseX, int mouseY, float partialTick, CallbackInfo ci) {
         var matrix = graphics.pose().last().pose();
         var center = new Vector2f(mouseX, mouseY);
-        var size = new Vector2f(100, 100);
+        var size = new Vector2f(200, 20);
         var color = new Vector4f(1, 1, 1, 0.7f);
         RenderQueue.draw(new RoundedRectangle(matrix, center, size, color, 10, 0), FilterTypes.GAUSSIAN_BLUR);
     }
--- src/main/resources/assets/advancedui/shaders/core/rounded_rect.fsh
+++ src/main/resources/assets/advancedui/shaders/core/rounded_rect.fsh
@@ -12,20 +12,25 @@ uniform vec2 Size;
 
 out vec4 fragColor;
 
+float insideBox(vec2 offset, vec2 size, vec2 pos) {
+    vec2 s = step(offset + vec2(0.0, size.y), pos) - step(offset + vec2(size.x, 0.0), pos);
+    return s.x * s.y;
+}
+
 float roundedRectangle(vec2 offset, vec2 pos, vec2 size, float radius, float thickness)
 {
     float d = length(max(abs(offset - pos), size) - size) - radius;
-    return 1.0 - smoothstep(thickness, thickness + 0.01, d);
+    return 1.0 - smoothstep(thickness, thickness + 0, d);
 }
 
 void main()
 {
     vec2 npos = vertexUV.xy / Resolution.xy;
     float aspect = Resolution.x / Resolution.y;
-    vec2 ratio = vec2(aspect, 1.0);
+    vec2 ratio = vec2(1.0, 1.0);
     vec2 uv = (2.0 * npos - 1.0) * ratio;
 
-    vec2 size = Size / Resolution.x;
+    highp vec2 size = Size / Resolution.x;
     vec2 pos = (2.0 * Center / Resolution - 1.0) * ratio;
     float radius = Radius / Resolution.x;
     float thickness = Thickness;
