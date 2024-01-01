4bd4f9e1e98302d1e0b64f533844aa358f67220c
hackermdch <58121864+hackermdch@users.noreply.github.com>
2024-01-02 02:19:25 +0800

    Update render

--- src/main/java/com/primogemstudio/advancedui/mmd/entity/TestEntityRenderer.kt
+++ src/main/java/com/primogemstudio/advancedui/mmd/entity/TestEntityRenderer.kt
@@ -1,24 +1,15 @@
 package com.primogemstudio.advancedui.mmd.entity
 
-import com.mojang.blaze3d.vertex.*
-import com.mojang.math.Axis
+import com.mojang.blaze3d.vertex.PoseStack
 import net.minecraft.client.renderer.MultiBufferSource
-import net.minecraft.client.renderer.block.BlockRenderDispatcher
+import net.minecraft.client.renderer.RenderType
 import net.minecraft.client.renderer.entity.EntityRenderer
 import net.minecraft.client.renderer.entity.EntityRendererProvider
-import net.minecraft.client.renderer.entity.TntMinecartRenderer
-import net.minecraft.client.renderer.texture.OverlayTexture
 import net.minecraft.resources.ResourceLocation
-import net.minecraft.world.level.block.Blocks
 
 class TestEntityRenderer(context: EntityRendererProvider.Context) : EntityRenderer<TestEntity>(context) {
-    private val blockRenderer: BlockRenderDispatcher
     override fun getTextureLocation(entity: TestEntity): ResourceLocation {
-        TODO("Not yet implemented")
-    }
-
-    init {
-        blockRenderer = context.blockRenderDispatcher
+        return ResourceLocation("")
     }
 
     override fun render(
@@ -29,19 +20,10 @@ class TestEntityRenderer(context: EntityRendererProvider.Context) : EntityRender
         buffer: MultiBufferSource,
         packedLight: Int
     ) {
-        poseStack.pushPose()
-        poseStack.translate(0.0f, 0.5f, 0.0f)
-
-        poseStack.mulPose(Axis.YP.rotationDegrees(-90.0f))
-        poseStack.translate(-0.5f, -0.5f, 0.5f)
-        poseStack.mulPose(Axis.YP.rotationDegrees(90.0f))
-        this.blockRenderer.renderSingleBlock(
-            Blocks.DIAMOND_ORE.defaultBlockState(),
-            poseStack,
-            buffer,
-            packedLight,
-            OverlayTexture.NO_OVERLAY
-        )
-        poseStack.popPose()
+        val buf = buffer.getBuffer(RenderType.debugFilledBox())
+        val pose = poseStack.last()
+        buf.vertex(pose.pose(), 0f, 1f, 0f).color(1f, 0f, 0f, 1f).endVertex()
+        buf.vertex(pose.pose(), 1f, -1f, 0f).color(0f, 1f, 0f, 1f).endVertex()
+        buf.vertex(pose.pose(), -1f, -1f, 0f).color(0f, 0f, 1f, 1f).endVertex()
     }
 }
