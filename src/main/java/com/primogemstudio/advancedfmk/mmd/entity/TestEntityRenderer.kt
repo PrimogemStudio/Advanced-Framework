83376d6a85234e71a3363600307215fcf54ff9d6
Jack253-png <guoxiuchen20170402@163.com>
2024-01-20 18:20:19 +0800

    direct commit test

--- src/main/java/com/primogemstudio/advancedfmk/mmd/entity/TestEntityRenderer.kt
+++ src/main/java/com/primogemstudio/advancedfmk/mmd/entity/TestEntityRenderer.kt
@@ -1,12 +1,19 @@
 package com.primogemstudio.advancedfmk.mmd.entity
 
+import com.mojang.blaze3d.vertex.BufferBuilder
+import com.mojang.blaze3d.vertex.BufferVertexConsumer.normalIntValue
 import com.mojang.blaze3d.vertex.PoseStack
+import com.mojang.blaze3d.vertex.VertexConsumer
 import com.mojang.math.Axis
 import net.minecraft.client.renderer.MultiBufferSource
 import net.minecraft.client.renderer.entity.EntityRenderer
 import net.minecraft.client.renderer.entity.EntityRendererProvider
 import net.minecraft.client.renderer.texture.OverlayTexture
 import net.minecraft.resources.ResourceLocation
+import org.joml.Matrix3f
+import org.joml.Matrix4f
+import org.joml.Vector3f
+import org.joml.Vector4f
 
 class TestEntityRenderer(context: EntityRendererProvider.Context) : EntityRenderer<TestEntity>(context) {
     companion object {
@@ -40,13 +47,59 @@ class TestEntityRenderer(context: EntityRendererProvider.Context) : EntityRender
         val processed = entity.getProcessed()
         if (!enable_pipeline) for (i in processed.indices) {
             val r = processed[i]
-            buf.vertex(pstk, r[0], r[1], r[2]).color(clr).uv(r[3], r[4]).overlayCoords(OverlayTexture.NO_OVERLAY)
-                .uv2(packedLight).normal(nom, r[5], r[6], r[7]).endVertex()
+            buf.directCommit(pstk, nom, r[0], r[1], r[2], r[3], r[4], packedLight, r[5], r[6], r[7])
         }
         else for (i in processed.indices) {
             val r = processed[i]
-            buf.vertex(pstk, r[0], r[1], r[2]).uv(r[3], r[4]).uv2(packedLight).endVertex()
+            buf.directCommit(pstk, r[0], r[1], r[2], r[3], r[4], packedLight)
         }
         poseStack.popPose()
+    }
+}
+
+fun VertexConsumer.directCommit(mat: Matrix4f, x: Float, y: Float, z: Float, u: Float, v: Float, light: Int) {
+    with(this as BufferBuilder) {
+        val p = mat.transform(Vector4f(x, y, z, 1.0f))
+        putFloat(0, p.x)
+        putFloat(4, p.y)
+        putFloat(8, p.z)
+        nextElement()
+        putFloat(0, u)
+        putFloat(4, v)
+        nextElement()
+        putShort(0, (light and 0xFFFF).toShort())
+        putShort(2, (light shr 16 and 0xFFFF).toShort())
+        nextElement()
+        endVertex()
+    }
+}
+
+fun VertexConsumer.directCommit(mat: Matrix4f, nom: Matrix3f, x: Float, y: Float, z: Float, u: Float, v: Float, light: Int, nx: Float, ny: Float, nz: Float) {
+    with(this as BufferBuilder) {
+        val p = mat.transform(Vector4f(x, y, z, 1.0f))
+        val n = nom.transform(Vector3f(nx, ny, nz))
+        putFloat(0, p.x)
+        putFloat(4, p.y)
+        putFloat(8, p.z)
+        nextElement()
+        putByte(0, 0xFF.toByte())
+        putByte(1, 0xFF.toByte())
+        putByte(2, 0xFF.toByte())
+        putByte(3, 0xFF.toByte())
+        nextElement()
+        putFloat(0, u)
+        putFloat(4, v)
+        nextElement()
+        putShort(0, 0)
+        putShort(0, 10)
+        nextElement()
+        putShort(0, (light and 0xFFFF).toShort())
+        putShort(2, (light shr 16 and 0xFFFF).toShort())
+        nextElement()
+        putByte(0, normalIntValue(n.x))
+        putByte(1, normalIntValue(n.y))
+        putByte(2, normalIntValue(n.z))
+        nextElement()
+        endVertex()
     }
 }
