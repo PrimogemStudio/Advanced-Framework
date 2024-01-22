b8c2072e56131c80d6848a1d814893aa966c858f
Jack253-png <guoxiuchen20170402@163.com>
2024-01-22 08:02:40 +0800

    Function optimize

--- src/main/java/com/primogemstudio/advancedfmk/mixin/BufferBuilderMixin.java
+++ src/main/java/com/primogemstudio/advancedfmk/mixin/BufferBuilderMixin.java
@@ -1,16 +1,30 @@
 package com.primogemstudio.advancedfmk.mixin;
 
-import com.mojang.blaze3d.vertex.BufferBuilder;
-import com.mojang.blaze3d.vertex.BufferVertexConsumer;
-import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.blaze3d.vertex.*;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
-@Mixin(BufferBuilder.class)
-public abstract class BufferBuilderMixin implements BufferVertexConsumer {
-    @Shadow public abstract void nextElement();
+import java.nio.ByteBuffer;
 
+@Mixin(BufferBuilder.class)
+public abstract class BufferBuilderMixin extends DefaultedVertexConsumer implements BufferVertexConsumer {
     @Shadow public abstract void putFloat(int var1, float var2);
+
+    @Shadow private int vertices;
+
+    @Shadow private ByteBuffer buffer;
+
+    @Shadow private int nextElementByte;
+
+    @Shadow private VertexFormat.Mode mode;
+
+    @Shadow private VertexFormat format;
+
+    @Shadow private int elementIndex;
+
+    @Shadow @Nullable private VertexFormatElement currentElement;
+
     public VertexConsumer uv(float u, float v) {
         this.putFloat(0, u);
         this.putFloat(4, v);
@@ -50,5 +64,28 @@ public abstract class BufferBuilderMixin implements BufferVertexConsumer {
         this.putByte(2, BufferVertexConsumer.normalIntValue(z));
         this.nextElement();
         return this;
+    }
+
+    @Override
+    public void endVertex() {
+        ++this.vertices;
+        if (this.mode == VertexFormat.Mode.LINES || this.mode == VertexFormat.Mode.LINE_STRIP) {
+            int i = this.format.getVertexSize();
+            this.buffer.put(this.nextElementByte, this.buffer, this.nextElementByte - i, i);
+            this.nextElementByte += i;
+            ++this.vertices;
+        }
+    }
+
+    public void nextElement() {
+        this.elementIndex = (this.elementIndex + 1) % this.format.getElements().size();
+        this.nextElementByte += this.currentElement.getByteSize();
+        this.currentElement = this.format.getElements().get(this.elementIndex);
+        if (this.currentElement.getUsage() == VertexFormatElement.Usage.PADDING) {
+            this.nextElement();
+        }
+        else if (this.defaultColorSet && this.currentElement.getUsage() == VertexFormatElement.Usage.COLOR) {
+            color(this.defaultR, this.defaultG, this.defaultB, this.defaultA);
+        }
     }
 }
