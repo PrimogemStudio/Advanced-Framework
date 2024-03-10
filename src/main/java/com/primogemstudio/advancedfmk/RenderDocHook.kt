f8b88b0d120dcaeda66d96886f265f2c2a5344f1
hackermdch <58121864+hackermdch@users.noreply.github.com>
2024-03-10 19:01:07 +0800

    Fix bug

--- src/main/java/com/primogemstudio/advancedfmk/RenderDocHook.kt
+++ src/main/java/com/primogemstudio/advancedfmk/RenderDocHook.kt
@@ -6,7 +6,7 @@ import org.apache.logging.log4j.LogManager
 class RenderDocHook : PreLaunchEntrypoint {
     override fun onPreLaunch() {
         try {
-            System.load("C:/Program Files/RenderDoc/renderdoc.dll")
+            System.load("X:/Program Files/RenderDoc/renderdoc.dll")
         } catch (e: Throwable) {
             LogManager.getLogger().error(e.message, e)
         }
