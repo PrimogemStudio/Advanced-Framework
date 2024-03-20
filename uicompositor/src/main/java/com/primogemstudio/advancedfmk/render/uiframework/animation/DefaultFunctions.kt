dc08d4a7b6b5abceb8f1345109b8eadab4fcb2ea
Jack253-png <guoxiuchen20170402@163.com>
2024-03-20 19:41:39 +0800

    Finish Sinusoidal Animation functions

--- uicompositor/src/main/java/com/primogemstudio/advancedfmk/render/uiframework/animation/DefaultFunctions.kt
+++ uicompositor/src/main/java/com/primogemstudio/advancedfmk/render/uiframework/animation/DefaultFunctions.kt
@@ -1,5 +1,9 @@
 package com.primogemstudio.advancedfmk.render.uiframework.animation
 
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
+
 val Linear: DataGenerator = DataGenerator { it }
 val QuadraticIn: DataGenerator = DataGenerator { it * it }
 val QuadraticOut: DataGenerator = DataGenerator { - it * (it - 2) }
@@ -9,3 +13,7 @@ val QuadraticInOut: DataGenerator = DataGenerator {
     val i2 = i1 - 1
     -(i2 * (i2 - 2) - 1) / 2
 }
+
+val SinusoidalIn: DataGenerator = DataGenerator { 1 - cos(it * PI / 2) }
+val SinusoidalOut: DataGenerator = DataGenerator { sin(it * PI / 2) }
+val SinusoidalInOut: DataGenerator = DataGenerator { -(cos(PI * it) - 1) / 2 }
