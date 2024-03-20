125b3c32b626fe2ab9ff8cc9e045b1bcff3aa4ba
Jack253-png <guoxiuchen20170402@163.com>
2024-03-20 20:07:30 +0800

    type alias

--- uicompositor/src/main/java/com/primogemstudio/advancedfmk/render/uiframework/animation/DefaultFunctions.kt
+++ uicompositor/src/main/java/com/primogemstudio/advancedfmk/render/uiframework/animation/DefaultFunctions.kt
@@ -5,32 +5,34 @@ import kotlin.math.cos
 import kotlin.math.pow
 import kotlin.math.sin
 
-val Linear: DataGenerator = DataGenerator { it }
-val QuadraticIn: DataGenerator = DataGenerator { it * it }
-val QuadraticOut: DataGenerator = DataGenerator { - it * (it - 2) }
-val QuadraticInOut: DataGenerator = DataGenerator {
+typealias DG = DataGenerator
+
+val Linear: DG = DG { it }
+val QuadraticIn: DG = DG { it * it }
+val QuadraticOut: DG = DG { - it * (it - 2) }
+val QuadraticInOut: DG = DG {
     val i1 = it * 2
-    if (i1 < 1) return@DataGenerator i1 * i1
+    if (i1 < 1) return@DG i1 * i1
     val i2 = i1 - 1
     -(i2 * (i2 - 2) - 1) / 2
 }
 
-val SinusoidalIn: DataGenerator = DataGenerator { 1 - cos(it * PI / 2) }
-val SinusoidalOut: DataGenerator = DataGenerator { sin(it * PI / 2) }
-val SinusoidalInOut: DataGenerator = DataGenerator { -(cos(PI * it) - 1) / 2 }
+val SinusoidalIn: DG = DG { 1 - cos(it * PI / 2) }
+val SinusoidalOut: DG = DG { sin(it * PI / 2) }
+val SinusoidalInOut: DG = DG { -(cos(PI * it) - 1) / 2 }
 
-val ExponentialIn: DataGenerator = DataGenerator {
-    if (it == 0.0) return@DataGenerator 0.0
+val ExponentialIn: DG = DG {
+    if (it == 0.0) return@DG 0.0
     2.0.pow(10 * (it - 1))
 }
-val ExponentialOut: DataGenerator = DataGenerator {
-    if (it == 1.0) return@DataGenerator 1.0
+val ExponentialOut: DG = DG {
+    if (it == 1.0) return@DG 1.0
     1 - 2.0.pow(-10 * it)
 }
-val ExponentialInOut: DataGenerator = DataGenerator {
-    if (it == 0.0) return@DataGenerator 0.0
-    if (it == 1.0) return@DataGenerator 1.0
+val ExponentialInOut: DG = DG {
+    if (it == 0.0) return@DG 0.0
+    if (it == 1.0) return@DG 1.0
     val i1 = it * 2
-    if (i1 < 1) return@DataGenerator 2.0.pow(10 * (i1 - 1)) / 2
+    if (i1 < 1) return@DG 2.0.pow(10 * (i1 - 1)) / 2
     (2 - 2.0.pow(-10 * it)) / 2
 }
