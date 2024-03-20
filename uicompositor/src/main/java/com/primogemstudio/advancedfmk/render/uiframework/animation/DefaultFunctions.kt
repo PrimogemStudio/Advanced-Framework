4a69f557aaf1dc6be0462133ae918fbb78b274c7
Jack253-png <guoxiuchen20170402@163.com>
2024-03-20 19:35:25 +0800

    Fix QuadraticInOut

--- uicompositor/src/main/java/com/primogemstudio/advancedfmk/render/uiframework/animation/DefaultFunctions.kt
+++ uicompositor/src/main/java/com/primogemstudio/advancedfmk/render/uiframework/animation/DefaultFunctions.kt
@@ -4,12 +4,8 @@ val Linear: DataGenerator = DataGenerator { it }
 val QuadraticIn: DataGenerator = DataGenerator { it * it }
 val QuadraticOut: DataGenerator = DataGenerator { - it * (it - 2) }
 val QuadraticInOut: DataGenerator = DataGenerator {
-    /*double i1 = t / d * 2;
-    if (i1 < 1) return c / 2 * i1 * i1 + b;
-    double i2 = i1 - 1;
-    return -c / 2 * (i2 * (i2 - 2) - 1) + b;*/
     val i1 = it * 2
-    if (i1 < 1) it * it / 2
+    if (i1 < 1) return@DataGenerator i1 * i1
     val i2 = i1 - 1
     -(i2 * (i2 - 2) - 1) / 2
 }
