005e7f01631fe081df3a3cbdc28e056f38444a9d
Jack253-png <guoxiuchen20170402@163.com>
2024-03-26 21:39:37 +0800

    Func viewer

--- uicompositor/src/main/java/com/primogemstudio/advancedfmk/TestMain.kt
+++ uicompositor/src/main/java/com/primogemstudio/advancedfmk/TestMain.kt
@@ -1,13 +1,34 @@
 package com.primogemstudio.advancedfmk
 
 import com.primogemstudio.advancedfmk.render.uiframework.animation.*
+import java.awt.Color
+import java.awt.Graphics
+import javax.swing.JFrame
+import javax.swing.JPanel
 
 fun main() {
-    listOf(Linear, QuadraticIn, QuadraticOut, QuadraticInOut, SinusoidalIn, SinusoidalOut, SinusoidalInOut, ExponentialIn, ExponentialOut, ExponentialInOut, CircularIn, CircularOut, CircularInOut, CubicIn, CubicOut, CubicInOut, QuarticIn, QuarticOut, QuarticInOut, QuinticIn, QuinticOut, QuinticInOut, ElasticIn, ElasticOut, ElasticInOut, BackIn, BackOut, BackInOut, BounceOut, BounceIn, BounceInOut).forEach {
-        println(it::class)
+    val frame = JFrame()
+    frame.setLocation(200, 200)
+    frame.setSize(500, 500)
+    frame.add(FuncPanel())
+    frame.isVisible = true
+}
 
-        for (i in 0 .. 100) {
-            println("${i.toDouble() / 100.0} -> ${it.gen(i.toDouble() / 100.0)}")
+class FuncPanel: JPanel() {
+    override fun paint(g: Graphics?) {
+        g?.setXORMode(Color.BLACK)
+        g?.color = Color.BLACK
+        g?.setPaintMode()
+        g?.fillRect(0, 0, 10000, 10000)
+        val points = 1000
+        val xarr = IntArray(points)
+        val yarr = IntArray(points)
+        for (i in 0 ..< points) {
+            xarr[i] = ((i / points.toDouble()) * 500.0 + 50).toInt()
+            yarr[i] = 500 - (BackIn.gen(i / points.toDouble()) * 500.0).toInt() + 50
         }
+
+        g?.color = Color.WHITE
+        g?.drawPolyline(xarr, yarr, points)
     }
 }
