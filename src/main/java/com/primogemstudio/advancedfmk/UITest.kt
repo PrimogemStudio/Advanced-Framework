5a87101154ab15795b0a6d3b828961a484e5c9f1
hackermdch <58121864+hackermdch@users.noreply.github.com>
2024-03-31 18:23:57 +0800

    Fix bug

--- src/main/java/com/primogemstudio/advancedfmk/UITest.kt
+++ src/main/java/com/primogemstudio/advancedfmk/UITest.kt
@@ -2,12 +2,13 @@ package com.primogemstudio.advancedfmk
 
 import com.primogemstudio.advancedfmk.render.uiframework.Compositor
 import com.primogemstudio.advancedfmk.render.uiframework.ui.UICompound
-import java.io.InputStreamReader
-import java.util.stream.Collectors
 
 
 fun loadNew(): UICompound {
     return Compositor.parseNew(
-        InputStreamReader(Compositor::class.java.classLoader.getResourceAsStream("assets/advancedfmk/ui/starrail_chat_v1.json")).readLines().stream().collect(Collectors.joining("\n"))
+        String(
+            Compositor::class.java.classLoader.getResourceAsStream("assets/advancedfmk/ui/starrail_chat_v1.json")!!
+                .readAllBytes()
+        )
     )
 }
