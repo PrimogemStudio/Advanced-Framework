d52895ffd78f549f3b184d55da849dbd4b3ea71a
Jack253-png <guoxiuchen20170402@163.com>
2024-04-04 07:24:17 +0800

    FT_Outline_Decompose using

--- fontengine/src/main/java/com/primogemstudio/advancedfmk/client/FTTestMain.kt
+++ fontengine/src/main/java/com/primogemstudio/advancedfmk/client/FTTestMain.kt
@@ -1,7 +1,9 @@
 package com.primogemstudio.advancedfmk.client
 
+import org.joml.Vector2f
 import org.lwjgl.system.MemoryStack
 import org.lwjgl.util.freetype.FT_Face
+import org.lwjgl.util.freetype.FT_Outline_Funcs
 import org.lwjgl.util.freetype.FT_Vector
 import org.lwjgl.util.freetype.FreeType.*
 import java.awt.BasicStroke
@@ -9,8 +11,6 @@ import java.awt.Color
 import java.awt.Graphics
 import java.awt.Graphics2D
 import java.awt.geom.Ellipse2D
-import java.io.File
-import java.io.PrintStream
 import javax.swing.JFrame
 import javax.swing.JPanel
 import kotlin.math.pow
@@ -18,6 +18,10 @@ import kotlin.math.pow
 fun main() {
     fun i26p6tof(i: Int): Float {
         return i.toFloat() * (2.0.pow(-6).toFloat())
+    }
+    fun addrToVec(addr: Long): Vector2f {
+        val ft_vec = FT_Vector.create(addr)
+        return Vector2f(i26p6tof(ft_vec.x().toInt()), i26p6tof(ft_vec.y().toInt()))
     }
 
     var pLib: Long
@@ -28,7 +32,7 @@ fun main() {
         pLib = ptrBuff.get(0)
         FT_New_Face(
             pLib,
-            "G:\\Star Rail\\Game\\StarRail_Data\\StreamingAssets\\MiHoYoSDKRes\\HttpServerResources\\font\\zh-cn.ttf",
+            "/usr/share/fonts/StarRailFont.ttf",
             0,
             ptrBuff
         )
@@ -39,81 +43,13 @@ fun main() {
     val glyphIndex = FT_Get_Char_Index(fce, 'j'.code.toLong())
     FT_Load_Glyph(fce, glyphIndex, FT_LOAD_DEFAULT or FT_LOAD_NO_BITMAP)
     val outline = fce.glyph()?.outline()!!
-    println(outline.n_points())
+    val funcs = FT_Outline_Funcs.create()
+        .move_to { to, user -> println("moveto: ${addrToVec(to)}"); 0}
+        .line_to { to, user -> println("lineto: ${addrToVec(to)}"); 0 }
+        .conic_to { ct, to, user -> println("conicto: ${addrToVec(ct)}, ${addrToVec(to)}"); 0 }
+        .cubic_to { ct1, ct2, to, user -> println("cubicto: ${addrToVec(ct1)}, ${addrToVec(ct2)}, ${addrToVec(to)}"); 0 }
+    FT_Outline_Decompose(outline, funcs, 1)
 
-    val frame = JFrame()
-    frame.setLocation(200, 200)
-    frame.setSize(500, 500)
-    frame.add(object : JPanel() {
-        override fun paint(g: Graphics?) {
-            g as Graphics2D
-            val stroke = BasicStroke(10f);
-            g.stroke = stroke
-            g.color = Color.BLACK
-            for (it in outline.points()) {
-                g.fill(
-                    Ellipse2D.Float(
-                        i26p6tof(it.x().toInt()) * 10 + 100, i26p6tof(it.y().toInt()) * 10 + 200, 10f, 10f
-                    )
-                )
-            }
-        }
-    })
-    frame.isVisible = true
-
-    val f = File("output.csv")
-    f.delete()
-    f.createNewFile()
-    val ps = PrintStream(f)
-    for (a in outline.points()) {
-        ps.println("${a.x()},${a.y()}")
-    }
-
-    var limit = 0
-    var v_last = FT_Vector.create()
-    var v_control: FT_Vector
-    var v_start: FT_Vector
-
-    var point = 0
-    var first = 0
-    for (i in 0..<outline.n_contours()) {
-        val last = outline.contours()[i].toInt()
-        limit = i
-
-        v_start = outline.points()[first]
-        v_last = outline.points()[last]
-        v_control = v_start
-
-        point = first
-        val tags = outline.tags()[first]
-        var tag = FT_CURVE_TAG(tags.toInt())
-
-        val fpriX = i26p6tof(v_control.x().toInt())
-        val fpriY = -i26p6tof(v_control.y().toInt())
-
-        var startX = fpriX
-        var startY = fpriY
-
-        println("$fpriX, $fpriY")
-        while (point < limit) {
-            point++
-            limit++
-            tag = FT_CURVE_TAG(tags.toInt())
-            println(tag)
-            when (tag) {
-                FT_CURVE_TAG_ON -> {
-                    val pointd = outline.points()[point]
-                    val fEndX = i26p6tof(pointd.x().toInt())
-                    val fEndY = -i26p6tof(pointd.y().toInt())
-
-                    println("lineto: $startX, $startY, $fEndX, $fEndY")
-
-                    startX = fEndX
-                    startY = fEndY
-                }
-            }
-        }
-
-        first = last + 1
-    }
+    FT_Done_Face(fce)
+    FT_Done_FreeType(pLib)
 }
