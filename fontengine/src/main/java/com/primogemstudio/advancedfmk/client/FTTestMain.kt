0835c0dcd394e380aafe61e21ca69abf16f100d7
hackermdch <58121864+hackermdch@users.noreply.github.com>
2024-04-03 19:20:15 +0800

    Update bug

--- fontengine/src/main/java/com/primogemstudio/advancedfmk/client/FTTestMain.kt
+++ fontengine/src/main/java/com/primogemstudio/advancedfmk/client/FTTestMain.kt
@@ -1,21 +1,26 @@
 package com.primogemstudio.advancedfmk.client
 
-import org.lwjgl.PointerBuffer
+import org.lwjgl.system.MemoryStack
 import org.lwjgl.util.freetype.FT_Face
-import java.nio.ByteBuffer
 import org.lwjgl.util.freetype.FreeType.*
 
 fun main() {
-    val lib = PointerBuffer.create(ByteBuffer.allocate(2048))
-    val face = PointerBuffer.create(ByteBuffer.allocate(2048))
-    FT_Init_FreeType(lib)
-    FT_New_Face(lib.address(), "/usr/share/fonts/StarRailFont.ttf", 0, face)
-    val fce = FT_Face.create(face.address())
+    var pLib: Long
+    var pFace: Long
+    MemoryStack.stackPush().use { stack ->
+        val ptrBuff = stack.mallocPointer(1)
+        FT_Init_FreeType(ptrBuff)
+        pLib = ptrBuff.get(0)
+        FT_New_Face(pLib, "C:/Users/Hacker/Desktop/zh-cn.ttf", 0, ptrBuff)
+        pFace = ptrBuff.get(0)
+    }
+    val fce = FT_Face.create(pFace)
     FT_Set_Pixel_Sizes(fce, 0, 24)
-
     val glyphIndex = FT_Get_Char_Index(fce, 'i'.code.toLong())
     FT_Load_Glyph(fce, glyphIndex, FT_LOAD_DEFAULT or FT_LOAD_NO_BITMAP)
-    val outline = fce.glyph()?.outline()
-    println(outline?.n_points())
-    println(outline?.points())
+    val outline = fce.glyph()?.outline()!!
+    println(outline.n_points())
+    for (a in outline.points()) {
+        print("[${a.x()},${a.y()}]")
+    }
 }
