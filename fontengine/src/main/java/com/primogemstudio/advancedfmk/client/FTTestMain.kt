5372eb3b033ffaca398a6062e39156bb74806458
hackermdch <58121864+hackermdch@users.noreply.github.com>
2024-04-04 09:33:14 +0800

    Update format

--- fontengine/src/main/java/com/primogemstudio/advancedfmk/client/FTTestMain.kt
+++ fontengine/src/main/java/com/primogemstudio/advancedfmk/client/FTTestMain.kt
@@ -22,9 +22,11 @@ fun main() {
     fun i26p6tof(i: Int): Float {
         return i.toFloat() * (2.0.pow(-6).toFloat())
     }
+
     fun i16p16tof(i: Int): Float {
         return i.toFloat() * (2.0.pow(-16).toFloat())
     }
+
     fun addrToVec(addr: Long): Vector2f {
         val ft_vec = FT_Vector.create(addr)
         return Vector2f(i26p6tof(ft_vec.x().toInt()) * 20f + 20f, i26p6tof(ft_vec.y().toInt()) * 20f + 20f)
@@ -38,7 +40,7 @@ fun main() {
         pLib = ptrBuff.get(0)
         FT_New_Face(
             pLib,
-            "/usr/share/fonts/StarRailFont.ttf",
+            "G:\\Star Rail\\Game\\StarRail_Data\\StreamingAssets\\MiHoYoSDKRes\\HttpServerResources\\font\\zh-cn.ttf",
             0,
             ptrBuff
         )
@@ -52,40 +54,39 @@ fun main() {
     FT_Set_Pixel_Sizes(fce, 0, 24)
     FT_Set_Transform(
         fce,
-        FT_Matrix.create()
-            .xx(1L * multiplier)
-            .xy(0L * multiplier)
-            .yx(0L * multiplier)
-            .yy(-1L * multiplier),
-        FT_Vector.create()
-            .x(0L)
-            .y(1100L)
+        FT_Matrix.create().xx(1L * multiplier).xy(0L * multiplier).yx(0L * multiplier).yy(-1L * multiplier),
+        FT_Vector.create().x(0L).y(1100L)
     )
-    val glyphIndex = FT_Get_Char_Index(fce, '2'.code.toLong())
+
+    val glyphIndex = FT_Get_Char_Index(fce, 'j'.code.toLong())
     FT_Load_Glyph(fce, glyphIndex, FT_LOAD_DEFAULT or FT_LOAD_NO_BITMAP)
     val outline = fce.glyph()?.outline()!!
-    val funcs = FT_Outline_Funcs.create()
-        .move_to { to, user -> oplist.add(Operation(
-            type = OpType.MOVE,
-            target = addrToVec(to)
-        )); 0}
-        .line_to { to, user -> oplist.add(Operation(
-            type = OpType.LINE,
-            target = addrToVec(to)
-        )); 0 }
-        .conic_to { ct, to, user -> oplist.add(Operation(
-            type = OpType.CONIC,
-            target = addrToVec(to),
-            control1 = addrToVec(ct)
-        )); 0 }
-        .cubic_to { ct1, ct2, to, user -> oplist.add(Operation(
-            type = OpType.CONIC,
-            target = addrToVec(to),
-            control1 = addrToVec(ct1),
-            control2 = addrToVec(ct2)
-        )); 0 }
+    val funcs = FT_Outline_Funcs.create().move_to { to, user ->
+        oplist.add(
+            Operation(
+                type = OpType.MOVE, target = addrToVec(to)
+            )
+        ); 0
+    }.line_to { to, user ->
+        oplist.add(
+            Operation(
+                type = OpType.LINE, target = addrToVec(to)
+            )
+        ); 0
+    }.conic_to { ct, to, user ->
+        oplist.add(
+            Operation(
+                type = OpType.CONIC, target = addrToVec(to), control1 = addrToVec(ct)
+            )
+        ); 0
+    }.cubic_to { ct1, ct2, to, user ->
+        oplist.add(
+            Operation(
+                type = OpType.CONIC, target = addrToVec(to), control1 = addrToVec(ct1), control2 = addrToVec(ct2)
+            )
+        ); 0
+    }
     FT_Outline_Decompose(outline, funcs, 1)
-
     FT_Done_Face(fce)
     FT_Done_FreeType(pLib)
 
@@ -107,7 +108,7 @@ fun main() {
                     OpType.LINE -> g.drawLine(pos.x.toInt(), pos.y.toInt(), it.target.x.toInt(), it.target.y.toInt())
                     OpType.CUBIC -> g.drawLine(pos.x.toInt(), pos.y.toInt(), it.target.x.toInt(), it.target.y.toInt())
                     OpType.CONIC -> {
-                        for (i in 1 ..< pri) {
+                        for (i in 1..<pri) {
                             val t = conic(pos, it.control1!!, it.target, i.toFloat() / pri)
                             val t2 = conic(pos, it.control1, it.target, (i - 1).toFloat() / pri)
                             g.drawLine(t.x.toInt(), t.y.toInt(), t2.x.toInt(), t2.y.toInt())
