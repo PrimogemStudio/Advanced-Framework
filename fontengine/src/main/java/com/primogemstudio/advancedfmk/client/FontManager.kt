12ed2533577d7f0cd8f7efbd92e9edc819c1b7ee
Jack253-png <guoxiuchen20170402@163.com>
2024-04-17 16:40:20 +0800

    Increase precision

--- fontengine/src/main/java/com/primogemstudio/advancedfmk/client/FontManager.kt
+++ fontengine/src/main/java/com/primogemstudio/advancedfmk/client/FontManager.kt
@@ -5,21 +5,21 @@ import com.mojang.blaze3d.vertex.VertexConsumer
 import com.primogemstudio.advancedfmk.ftwrap.DefaultFont
 import com.primogemstudio.advancedfmk.ftwrap.vtxf.CharGlyph
 import com.primogemstudio.advancedfmk.ftwrap.vtxf.CharacterMap
+import org.apache.logging.log4j.LogManager
 
 object FontManager {
+    private val LOGGER = LogManager.getLogger(FontManager::class.java)
     private val characterMap = CharacterMap()
 
     var CurrentFont = DefaultFont.FONT
 
     init {
-        val preload = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ?/[]{};:'\"=-+()!@#$%^&*_<>.,~`"
-        for (c in preload) {
-            characterMap.put(c, CurrentFont, 15)
-        }
+        for (c in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ?/[]{};:'\"=-+()!@#$%^&*_<>.,~`") characterMap.put(c, CurrentFont, 50)
     }
 
     private fun loadChar(char: Char): CharGlyph {
-        characterMap.put(char, CurrentFont, 15)
+        LOGGER.info("Loading char $char")
+        characterMap.put(char, CurrentFont, 50)
         return characterMap[char]!!
     }
 
@@ -30,7 +30,7 @@ object FontManager {
             for (ind in glyph.indices) {
                 val v = glyph.vertices[ind]
                 poseStack.pushPose()
-                poseStack.scale(30f, 30f, 1f)
+                poseStack.scale(10f, 10f, 1f)
                 poseStack.translate(x, 3.5, 0.0)
                 buff.vertex(poseStack.last().pose(), v.x, v.y, 0f).color(255, 255, 255, 255).endVertex()
                 poseStack.popPose()
