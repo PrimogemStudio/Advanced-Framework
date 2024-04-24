8bc5a0f5e44166dfc7ff4427af3c8c944e32fd9f
Jack253-png <guoxiuchen20170402@163.com>
2024-04-24 18:51:18 +0800

    Add BufferManager.kt

---
+++ fontengine/src/main/java/com/primogemstudio/advancedfmk/fontengine/BufferManager.kt
@@ -0,0 +1,48 @@
+package com.primogemstudio.advancedfmk.fontengine
+
+import com.mojang.blaze3d.pipeline.TextureTarget
+import com.mojang.blaze3d.systems.RenderSystem
+import com.mojang.blaze3d.vertex.*
+import com.primogemstudio.advancedfmk.fontengine.Shaders.TEXT_BLUR
+import net.minecraft.Util
+import net.minecraft.Util.OS
+import net.minecraft.client.Minecraft
+import net.minecraft.client.gui.GuiGraphics
+import net.minecraft.client.renderer.GameRenderer
+
+object BufferManager {
+    val fontInternal = TextureTarget(1, 1, true, Util.getPlatform() == OS.OSX).apply { setClearColor(1f, 1f, 1f, 0f) }
+    fun updateBufferColor(color: Int) {
+        fontInternal.setClearColor(
+            color.shr(16).and(0xff).toFloat() / 255f,
+            color.shr(8).and(0xff).toFloat() / 255f,
+            color.and(0xff).toFloat() / 255f,
+            0f
+        )
+    }
+    inline fun renderText(call: (VertexConsumer, PoseStack) -> Unit, graphics: GuiGraphics, partialTick: Float) {
+        fontInternal.clear(Util.getPlatform() === OS.OSX)
+        fontInternal.resize(
+            Minecraft.getInstance().window.width * 4,
+            Minecraft.getInstance().window.height * 4,
+            Util.getPlatform() === OS.OSX
+        )
+        fontInternal.bindWrite(true)
+        RenderSystem.setShader { GameRenderer.getPositionColorShader() }
+        val buff = Tesselator.getInstance().builder
+        val scale = Minecraft.getInstance().window.guiScale.toFloat()
+        val poseStack: PoseStack = graphics.pose()
+        poseStack.pushPose()
+        poseStack.scale(1 / scale, 1 / scale, 1f)
+        buff.begin(VertexFormat.Mode.TRIANGLES, DefaultVertexFormat.POSITION_COLOR)
+        poseStack.popPose()
+        call(buff, poseStack)
+        RenderSystem.enableBlend()
+        RenderSystem.disableCull()
+        BufferUploader.drawWithShader(buff.end())
+        RenderSystem.enableCull()
+        RenderSystem.disableBlend()
+        TEXT_BLUR.setSamplerUniform("BaseLayer", fontInternal)
+        TEXT_BLUR.render(partialTick)
+    }
+}
--- src/main/java/com/primogemstudio/advancedfmk/mixin/TitleScreenFontEngineMixin.java
+++ src/main/java/com/primogemstudio/advancedfmk/mixin/TitleScreenFontEngineMixin.java
@@ -1,18 +1,9 @@
 package com.primogemstudio.advancedfmk.mixin;
 
-import com.mojang.blaze3d.pipeline.TextureTarget;
-import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.vertex.BufferUploader;
-import com.mojang.blaze3d.vertex.DefaultVertexFormat;
-import com.mojang.blaze3d.vertex.Tesselator;
-import com.mojang.blaze3d.vertex.VertexFormat;
+import com.primogemstudio.advancedfmk.fontengine.BufferManager;
 import com.primogemstudio.advancedfmk.fontengine.ComposedFont;
-import com.primogemstudio.advancedfmk.fontengine.Shaders;
-import net.minecraft.Util;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.TitleScreen;
-import net.minecraft.client.renderer.GameRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -22,33 +13,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(TitleScreen.class)
 public class TitleScreenFontEngineMixin {
     @Unique
-    private static final TextureTarget fontInternal = new TextureTarget(1, 1, true, Util.getPlatform() == Util.OS.OSX);
-    @Unique
     private static final ComposedFont font = new ComposedFont();
-    static {
-        fontInternal.setClearColor(1f, 1f, 1f, 0f);
-    }
 
     @Inject(at = @At("RETURN"), method = "render")
     public void render(GuiGraphics graphics, int mouseX, int mouseY, float partialTick, CallbackInfo ci) {
-        fontInternal.clear(Util.getPlatform() == Util.OS.OSX);
-        fontInternal.resize(Minecraft.getInstance().getWindow().getWidth() * 4, Minecraft.getInstance().getWindow().getHeight() * 4, Util.getPlatform() == Util.OS.OSX);
-        fontInternal.bindWrite(true);
-        RenderSystem.setShader(GameRenderer::getPositionColorShader);
-        var buff = Tesselator.getInstance().getBuilder();
-        var scale = (float) Minecraft.getInstance().getWindow().getGuiScale();
-        var poseStack = graphics.pose();
-        poseStack.pushPose();
-        poseStack.scale(1 / scale, 1 / scale, 1);
-        buff.begin(VertexFormat.Mode.TRIANGLES, DefaultVertexFormat.POSITION_COLOR);
-        poseStack.popPose();
-        font.drawWrapText(buff, poseStack, "测试abcd？?!", 200, 200, 9, 25, 0xffffffff);
-        RenderSystem.enableBlend();
-        RenderSystem.disableCull();
-        BufferUploader.drawWithShader(buff.end());
-        RenderSystem.enableCull();
-        RenderSystem.disableBlend();
-        Shaders.INSTANCE.getTEXT_BLUR().setSamplerUniform("BaseLayer", fontInternal);
-        Shaders.INSTANCE.getTEXT_BLUR().render(partialTick);
+        BufferManager.INSTANCE.updateBufferColor(0xffff77ff);
+        BufferManager.INSTANCE.renderText((vertexConsumer, poseStack) -> {
+            font.drawWrapText(vertexConsumer, poseStack, "测试abcd？?!", 200, 200, 9, 25, 0xffff77ff);
+            return null;
+        }, graphics, partialTick);
     }
 }
