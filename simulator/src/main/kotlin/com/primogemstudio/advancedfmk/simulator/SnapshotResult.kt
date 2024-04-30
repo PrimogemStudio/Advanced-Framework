a4ec8f3acf2d98e54951ae6fa8e9ddf79371ff7a
Jack253-png <guoxiuchen20170402@163.com>
2024-04-30 22:05:41 +0800

    Add snapshot result call

--- simulator/src/main/kotlin/com/primogemstudio/advancedfmk/simulator/SnapshotResult.kt
+++ simulator/src/main/kotlin/com/primogemstudio/advancedfmk/simulator/SnapshotResult.kt
@@ -7,4 +7,7 @@ data class SnapshotResult(
     val enemiesData: List<Map<String, Any>>,
     val operQueue: List<MutableList<BasicRoundtripCharacter>>,
     val lastOperation: AttackResult?
-)
+) {
+    fun finish(): Boolean = charactersData.map { it["health"] as Float }.sum() == 0f || win()
+    fun win(): Boolean = enemiesData.map { it["health"] as Float }.sum() == 0f
+}
