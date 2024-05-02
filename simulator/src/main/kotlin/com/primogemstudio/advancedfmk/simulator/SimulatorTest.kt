a005a558d38314a00dc377db9121ff190d248ac1
Jack253-png <guoxiuchen20170402@163.com>
2024-05-02 11:58:43 +0800

    Update test

--- simulator/src/main/kotlin/com/primogemstudio/advancedfmk/simulator/SimulatorTest.kt
+++ simulator/src/main/kotlin/com/primogemstudio/advancedfmk/simulator/SimulatorTest.kt
@@ -43,13 +43,13 @@ fun main() {
 
     val uni = SimulatedUniverse(
         listOf(
-            RoundtripCharacterImplv0("Test character 1", 100f, 25f),
-            RoundtripCharacterImplv0("Test character 2", 200f, 15f),
-            RoundtripCharacterImplv0("Test character 3", 50f, 50f)
+            RoundtripCharacterImplv0("Test character 1", 100f, 25f * 0.7f),
+            RoundtripCharacterImplv0("Test character 2", 200f, 15f * 0.7f),
+            RoundtripCharacterImplv0("Test character 3", 50f, 50f * 0.7f)
         ),
         listOf(
-            RoundtripCharacterImplv0("Test enemy 1", 50f * 2f, 20f),
-            RoundtripCharacterImplv0("Test enemy 2", 75f * 2f, 20f)
+            RoundtripCharacterImplv0("Test enemy 1", 50f * 2.3f, 20f),
+            RoundtripCharacterImplv0("Test enemy 2", 75f * 2.3f, 20f)
         )
     )
 
