156f83a40a4cd2dfc6585dea7738079800448825
Jack253-png <guoxiuchen20170402@163.com>
2024-05-19 21:27:54 +0800

    jsengine module

---
+++ jsengine/src/main/kotlin/com/primogemstudio/jsengine/JavascriptWrapper.kt
@@ -0,0 +1,31 @@
+package com.primogemstudio.jsengine
+
+import javax.script.Bindings
+import javax.script.Invocable
+import javax.script.ScriptContext
+import javax.script.ScriptEngineManager
+
+
+class JavascriptWrapper {
+    companion object {
+        @JvmStatic
+        fun main(arr: Array<String>) {
+            val engineManager = ScriptEngineManager()
+            val jsEngine = engineManager.getEngineByName("graal.js")
+            val funcCall = jsEngine as Invocable
+
+            val bindings: Bindings = jsEngine.getBindings(ScriptContext.ENGINE_SCOPE)
+            bindings.put("polyglot.js.allowHostAccess", true)
+            bindings.put("polyglot.js.allowHostClassLookup", true)
+
+            jsEngine.eval(JavascriptWrapper::class.java.classLoader.getResource("test.js")?.readText() ?: "")
+
+            val jsonObject = mutableMapOf(Pair("a", "abc"))
+
+            val res = funcCall.invokeFunction("App", jsonObject)
+            println("invoke param: $jsonObject")
+            println("invoke result type: " + res.javaClass.name)
+            println("invoke result: ${(res as Map<*, *>)["msg"]}")
+        }
+    }
+}
---
+++ jsengine/src/main/resources/test.js
@@ -0,0 +1,6 @@
+const App = p => {
+    p.a = "def"
+    let r = {}
+    r.msg = p
+    return r
+}
