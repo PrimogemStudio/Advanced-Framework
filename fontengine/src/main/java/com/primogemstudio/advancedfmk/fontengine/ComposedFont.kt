67bd174e7496dfb954e9bce00134ced6e66ecaf2
Jack253-png <guoxiuchen20170402@163.com>
2024-05-26 07:38:13 +0800

    decrease glyph precision

--- fontengine/src/main/java/com/primogemstudio/advancedfmk/fontengine/ComposedFont.kt
+++ fontengine/src/main/java/com/primogemstudio/advancedfmk/fontengine/ComposedFont.kt
@@ -17,7 +17,7 @@ class ComposedFont {
         for (c in 0..128) {
             fontStack.forEach {
                 try {
-                    characterMap.put(c.toChar(), it, 50)
+                    characterMap.put(c.toChar(), it, 20)
                 } catch (_: Exception) {
                 }
             }
