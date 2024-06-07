d176ddcd7747700e85ecaef182841ce17df71090
Jack253-png <guoxiuchen20170402@163.com>
2024-06-07 23:20:25 +0800

    Update test

---
+++ src/test/kotlin/com/primogemstudio/advancedfmk/tests/SimulatorTest.kt
@@ -0,0 +1,10 @@
+package com.primogemstudio.advancedfmk.tests
+
+import org.junit.jupiter.api.Test
+
+internal class SimulatorTest {
+    @Test
+    fun binOutputTest() {
+        println("Test!")
+    }
+}
