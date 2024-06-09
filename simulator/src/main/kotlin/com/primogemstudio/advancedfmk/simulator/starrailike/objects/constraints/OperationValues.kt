0600bcad31dd7d81efdb34190776ea06aa3ecbaa
Jack253-png <guoxiuchen20170402@163.com>
2024-06-09 16:52:45 +0800

    Redefine constraints

--- simulator/src/main/kotlin/com/primogemstudio/advancedfmk/simulator/starrailike/objects/constraints/OperationValues.kt
+++ simulator/src/main/kotlin/com/primogemstudio/advancedfmk/simulator/starrailike/objects/constraints/OperationValues.kt
@@ -1,4 +1,4 @@
 package com.primogemstudio.advancedfmk.simulator.starrailike.objects.constraints
 
-const val OPSTK_FLAG_INSERTED = 2147483648u
-const val TURN_LENGTH = 10000u
+const val OPSTK_FLAG_INSERTED = 0x80000000u
+const val TURN_LENGTH = 0x2710u
