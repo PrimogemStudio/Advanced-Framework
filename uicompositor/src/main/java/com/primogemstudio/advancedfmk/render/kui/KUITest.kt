da469a91746f952565ff24f158db03cc3c3e31c0
Jack253-png <guoxiuchen20170402@163.com>
2024-07-04 13:30:06 +0800

    Update text

--- uicompositor/src/main/java/com/primogemstudio/advancedfmk/render/kui/KUITest.kt
+++ uicompositor/src/main/java/com/primogemstudio/advancedfmk/render/kui/KUITest.kt
@@ -23,8 +23,7 @@ class KUITest {
                 Vector2f(0f, 0f),
                 "测试！Hello world from UI compositor!",
                 Vector4f(1f),
-                9,
-                true
+                9
             )
         )
     )
