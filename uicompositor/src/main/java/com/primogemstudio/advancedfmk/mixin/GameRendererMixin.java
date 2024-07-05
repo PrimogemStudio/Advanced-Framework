b18a58384c8c32039dd8fe7cd509e507df61fb46
Jack253-png <guoxiuchen20170402@163.com>
2024-07-05 20:31:54 +0800

    Update

--- uicompositor/src/main/java/com/primogemstudio/advancedfmk/mixin/GameRendererMixin.java
+++ uicompositor/src/main/java/com/primogemstudio/advancedfmk/mixin/GameRendererMixin.java
@@ -1,20 +1,36 @@
 package com.primogemstudio.advancedfmk.mixin;
 
 import com.primogemstudio.advancedfmk.render.uiframework.ui.GlobalVarsKt;
+import net.minecraft.Util;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GameRenderer;
 import org.joml.Vector2f;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import static com.primogemstudio.advancedfmk.render.kui.pipe.IndeterminateVarsKt.composeFrame;
+
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
+    @Unique
+    private static int width, height = 0;
+
     @Inject(method = "render", at = @At(value = "INVOKE", shift = At.Shift.AFTER, target = "Lnet/minecraft/client/gui/GuiGraphics;<init>(Lnet/minecraft/client/Minecraft;Lnet/minecraft/client/renderer/MultiBufferSource$BufferSource;)V"))
     private void preRender(DeltaTracker deltaTracker, boolean bl, CallbackInfo ci) {
         GlobalVarsKt.getData().setScreen_size(new Vector2f(Minecraft.getInstance().getWindow().getGuiScaledWidth(), Minecraft.getInstance().getWindow().getGuiScaledHeight()));
         GlobalVarsKt.getData().setTick(deltaTracker.getGameTimeDeltaPartialTick(false));
+
+        int w = Minecraft.getInstance().getWindow().getGuiScaledWidth();
+        int h = Minecraft.getInstance().getWindow().getGuiScaledHeight();
+
+        if (width != w || height != h) {
+            width = w;
+            height = h;
+            composeFrame().resize(w, h, Util.getPlatform() == Util.OS.OSX);
+        }
     }
 }
--- uicompositor/src/main/java/com/primogemstudio/advancedfmk/render/kui/pipe/IndeterminateVars.kt
+++ uicompositor/src/main/java/com/primogemstudio/advancedfmk/render/kui/pipe/IndeterminateVars.kt
@@ -3,4 +3,5 @@ package com.primogemstudio.advancedfmk.render.kui.pipe
 import com.mojang.blaze3d.pipeline.TextureTarget
 import net.minecraft.Util
 
-var COMPOSE_FRAME = TextureTarget(1, 1, true, Util.getPlatform() == Util.OS.OSX)
+val COMPOSE_FRAME = TextureTarget(1, 1, true, Util.getPlatform() == Util.OS.OSX)
+fun composeFrame(): TextureTarget = COMPOSE_FRAME
