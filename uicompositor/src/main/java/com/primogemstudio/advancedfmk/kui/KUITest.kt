930efd752b20091b171ddaa4cff24539f3134250
Jack253-png <guoxiuchen20170402@163.com>
2024-07-09 15:31:10 +0800

    Update class file

--- uicompositor/src/main/java/com/primogemstudio/advancedfmk/kui/KUITest.kt
+++ uicompositor/src/main/java/com/primogemstudio/advancedfmk/kui/KUITest.kt
@@ -16,8 +16,6 @@ import org.objectweb.asm.ClassWriter
 import org.objectweb.asm.Opcodes.*
 import org.objectweb.asm.tree.ClassNode
 import org.objectweb.asm.tree.MethodNode
-import java.nio.file.Files
-import java.nio.file.Path
 
 class KUITest {
     val elem = GroupElement(
@@ -46,25 +44,9 @@ class KUITest {
 }
 
 fun main() {
-    val parser = QMLParser(CommonTokenStream(QMLLexer(CharStreams.fromString("import QtQuick 2.0\nRectangle { }"))))
+    val parser =
+        QMLParser(CommonTokenStream(QMLLexer(CharStreams.fromString("import QtQuick 2.0\nRectangle { color = \"cyan\" }"))))
     println(parser.program().toStringTree())
-
-    /*ClassNode cn = new ClassNode();
-        cn.version = V1_5;
-        cn.access = ACC_PUBLIC + ACC_ABSTRACT + ACC_INTERFACE;
-        cn.name = "pkg/Comparable";
-        cn.superName = "java/lang/Object";
-        cn.interfaces.add("pkg/Mesurable");
-        cn.fields.add(new FieldNode(ACC_PUBLIC + ACC_FINAL + ACC_STATIC,
-                "LESS", "Ljava/lang/String;", null, "sss"));
-        cn.fields.add(new FieldNode(ACC_PUBLIC + ACC_FINAL + ACC_STATIC,
-                "EQUAL", "I", null, new Integer(0)));
-        cn.fields.add(new FieldNode(ACC_PUBLIC + ACC_FINAL + ACC_STATIC,
-                "GREATER", "I", null, new Integer(1)));
-        cn.methods.add(new MethodNode(ACC_PUBLIC + ACC_ABSTRACT,
-                "compareTo", "(Ljava/lang/Object;)I", null, null));
-        ClassWriter cw=new ClassWriter(ClassWriter.COMPUTE_MAXS);
-        cn.accept(cw);*/
 
     val cn = ClassNode()
     cn.access = ACC_PUBLIC
@@ -107,8 +89,6 @@ fun main() {
 
     val cld = MyClassLoader()
     val c = cld.defineClass("com.primogemstudio.advancedfmk.kui.Dyn", cw.toByteArray())
-
-    Files.write(Path.of("/home/coder2/Advanced-Framework/Test.class"), cw.toByteArray())
 
     val r = c.getMethod("test")
     val cs = c.getConstructor()
