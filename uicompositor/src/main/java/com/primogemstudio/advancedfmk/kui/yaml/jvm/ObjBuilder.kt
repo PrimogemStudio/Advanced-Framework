c6961de4a6d05a1bb33fbddfcedd9a42ad675dfe
Jack253-png <guoxiuchen20170402@163.com>
2024-07-12 15:13:59 +0800

    Update

--- uicompositor/src/main/java/com/primogemstudio/advancedfmk/kui/yaml/jvm/ObjBuilder.kt
+++ uicompositor/src/main/java/com/primogemstudio/advancedfmk/kui/yaml/jvm/ObjBuilder.kt
@@ -24,94 +24,161 @@ class ObjBuilder(val root: UIRoot): ClassLoader() {
             "<init>", "()V", null, null
         )
         mn.visitCode()
+        // Super init
         mn.visitVarInsn(ALOAD, 0)
         mn.visitMethodInsn(INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false)
 
+        // stdout
         mn.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;")
         mn.visitLdcInsn("Test")
         mn.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false)
 
+        // Push stack (this)
         mn.visitVarInsn(ALOAD, 0)
+        // New instance
         mn.visitTypeInsn(NEW, "com/primogemstudio/advancedfmk/kui/elements/GroupElement")
         mn.visitInsn(DUP)
+        // arg 1
         mn.visitLdcInsn("main")
+        // arg 2 - array length
         mn.visitInsn(ICONST_2)
+        // arg 2 - array
         mn.visitTypeInsn(ANEWARRAY, "com/primogemstudio/advancedfmk/kui/elements/RealElement")
+        // ???
         mn.visitVarInsn(ASTORE, 1)
         mn.visitVarInsn(ALOAD, 1)
-        mn.visitInsn(ICONST_0)
-        mn.visitTypeInsn(NEW, "com/primogemstudio/advancedfmk/kui/elements/RectangleElement")
-        mn.visitInsn(DUP)
-        mn.visitLdcInsn("test")
+        run {
+            // array[0] construct
+            mn.visitInsn(ICONST_0)
 
-        mn.visitTypeInsn(NEW, "org/joml/Vector2f")
-        mn.visitInsn(DUP)
-        mn.visitInsn(FCONST_0)
-        mn.visitInsn(FCONST_0)
-        mn.visitMethodInsn(INVOKESPECIAL, "org/joml/Vector2f", "<init>", "(FF)V", false)
+            // New instance
+            mn.visitTypeInsn(NEW, "com/primogemstudio/advancedfmk/kui/elements/RectangleElement")
+            mn.visitInsn(DUP)
+            // arg 1
+            mn.visitLdcInsn("test")
 
-        mn.visitTypeInsn(NEW, "org/joml/Vector2f")
-        mn.visitInsn(DUP)
-        mn.visitLdcInsn(100f)
-        mn.visitLdcInsn(100f)
-        mn.visitMethodInsn(INVOKESPECIAL, "org/joml/Vector2f", "<init>", "(FF)V", false)
+            // arg 2
+            mn.visitTypeInsn(NEW, "org/joml/Vector2f")
+            mn.visitInsn(DUP)
+            mn.visitInsn(FCONST_0)
+            mn.visitInsn(FCONST_0)
+            mn.visitMethodInsn(INVOKESPECIAL, "org/joml/Vector2f", "<init>", "(FF)V", false)
 
-        mn.visitTypeInsn(NEW, "org/joml/Vector4f")
-        mn.visitInsn(DUP)
-        mn.visitInsn(FCONST_1)
-        mn.visitInsn(FCONST_1)
-        mn.visitInsn(FCONST_1)
-        mn.visitLdcInsn(0.25f)
-        mn.visitMethodInsn(INVOKESPECIAL, "org/joml/Vector4f", "<init>", "(FFFF)V", false)
+            // arg 3
+            mn.visitTypeInsn(NEW, "org/joml/Vector2f")
+            mn.visitInsn(DUP)
+            mn.visitLdcInsn(100f)
+            mn.visitLdcInsn(100f)
+            mn.visitMethodInsn(INVOKESPECIAL, "org/joml/Vector2f", "<init>", "(FF)V", false)
 
-        mn.visitLdcInsn(20f)
-        mn.visitInsn(FCONST_0)
+            // arg 4
+            mn.visitTypeInsn(NEW, "org/joml/Vector4f")
+            mn.visitInsn(DUP)
+            mn.visitInsn(FCONST_1)
+            mn.visitInsn(FCONST_1)
+            mn.visitInsn(FCONST_1)
+            mn.visitLdcInsn(0.25f)
+            mn.visitMethodInsn(INVOKESPECIAL, "org/joml/Vector4f", "<init>", "(FFFF)V", false)
 
-        mn.visitLdcInsn(0.006f)
-        mn.visitLdcInsn("advancedfmk:ui/textures/microsoft.png")
-        mn.visitMethodInsn(INVOKESTATIC, "net/minecraft/resources/ResourceLocation", "parse", "(Ljava/lang/String;)Lnet/minecraft/resources/ResourceLocation;", false)
+            // arg 5
+            mn.visitLdcInsn(20f)
+            // arg 6
+            mn.visitInsn(FCONST_0)
+            // arg 7
+            mn.visitLdcInsn(0.006f)
+            // arg 8
+            mn.visitLdcInsn("advancedfmk:ui/textures/microsoft.png")
 
-        mn.visitTypeInsn(NEW, "com/primogemstudio/advancedfmk/kui/pipe/PostShaderFilter")
-        mn.visitInsn(DUP)
-        mn.visitMethodInsn(INVOKESTATIC, "org/ladysnake/satin/api/managed/ShaderEffectManager", "getInstance", "()Lorg/ladysnake/satin/api/managed/ShaderEffectManager;", true)
-        mn.visitLdcInsn("shaders/filter/gaussian_blur.json")
-        mn.visitMethodInsn(INVOKESTATIC, "net/minecraft/resources/ResourceLocation", "withDefaultNamespace", "(Ljava/lang/String;)Lnet/minecraft/resources/ResourceLocation;", false)
-        mn.visitMethodInsn(INVOKEINTERFACE, "org/ladysnake/satin/api/managed/ShaderEffectManager", "manage", "(Lnet/minecraft/resources/ResourceLocation;)Lorg/ladysnake/satin/api/managed/ManagedShaderEffect;", true)
+            // arg 9
+            mn.visitMethodInsn(
+                INVOKESTATIC,
+                "net/minecraft/resources/ResourceLocation",
+                "parse",
+                "(Ljava/lang/String;)Lnet/minecraft/resources/ResourceLocation;",
+                false
+            )
 
-        mn.visitMethodInsn(INVOKESPECIAL, "com/primogemstudio/advancedfmk/kui/pipe/PostShaderFilter", "<init>", "(Lorg/ladysnake/satin/api/managed/ManagedShaderEffect;)V", false)
-        mn.visitTypeInsn(CHECKCAST, "com/primogemstudio/advancedfmk/kui/pipe/FilterBase")
-        mn.visitMethodInsn(INVOKESPECIAL, "com/primogemstudio/advancedfmk/kui/elements/RectangleElement", "<init>", "(Ljava/lang/String;Lorg/joml/Vector2f;Lorg/joml/Vector2f;Lorg/joml/Vector4f;FFFLnet/minecraft/resources/ResourceLocation;Lcom/primogemstudio/advancedfmk/kui/pipe/FilterBase;)V", false)
-        mn.visitInsn(AASTORE)
-        mn.visitVarInsn(ALOAD, 1)
-        mn.visitInsn(ICONST_1)
+            mn.visitTypeInsn(NEW, "com/primogemstudio/advancedfmk/kui/pipe/PostShaderFilter")
+            mn.visitInsn(DUP)
+            mn.visitMethodInsn(
+                INVOKESTATIC,
+                "org/ladysnake/satin/api/managed/ShaderEffectManager",
+                "getInstance",
+                "()Lorg/ladysnake/satin/api/managed/ShaderEffectManager;",
+                true
+            )
+            mn.visitLdcInsn("shaders/filter/gaussian_blur.json")
+            mn.visitMethodInsn(
+                INVOKESTATIC,
+                "net/minecraft/resources/ResourceLocation",
+                "withDefaultNamespace",
+                "(Ljava/lang/String;)Lnet/minecraft/resources/ResourceLocation;",
+                false
+            )
+            mn.visitMethodInsn(
+                INVOKEINTERFACE,
+                "org/ladysnake/satin/api/managed/ShaderEffectManager",
+                "manage",
+                "(Lnet/minecraft/resources/ResourceLocation;)Lorg/ladysnake/satin/api/managed/ManagedShaderEffect;",
+                true
+            )
 
-        mn.visitTypeInsn(NEW, "com/primogemstudio/advancedfmk/kui/elements/TextElement")
-        mn.visitInsn(DUP)
-        mn.visitLdcInsn("test")
-        mn.visitTypeInsn(NEW, "org/joml/Vector2f")
-        mn.visitInsn(DUP)
-        mn.visitInsn(FCONST_0)
-        mn.visitInsn(FCONST_0)
-        mn.visitMethodInsn(INVOKESPECIAL, "org/joml/Vector2f", "<init>", "(FF)V", false)
+            mn.visitMethodInsn(
+                INVOKESPECIAL,
+                "com/primogemstudio/advancedfmk/kui/pipe/PostShaderFilter",
+                "<init>",
+                "(Lorg/ladysnake/satin/api/managed/ManagedShaderEffect;)V",
+                false
+            )
+            mn.visitTypeInsn(CHECKCAST, "com/primogemstudio/advancedfmk/kui/pipe/FilterBase")
+            mn.visitMethodInsn(
+                INVOKESPECIAL,
+                "com/primogemstudio/advancedfmk/kui/elements/RectangleElement",
+                "<init>",
+                "(Ljava/lang/String;Lorg/joml/Vector2f;Lorg/joml/Vector2f;Lorg/joml/Vector4f;FFFLnet/minecraft/resources/ResourceLocation;Lcom/primogemstudio/advancedfmk/kui/pipe/FilterBase;)V",
+                false
+            )
+            // store array[0]
+            mn.visitInsn(AASTORE)
+            mn.visitVarInsn(ALOAD, 1)
+        }
 
-        mn.visitLdcInsn("测试！Hello world from UI compositor!")
-        mn.visitTypeInsn(NEW, "org/joml/Vector4f")
-        mn.visitInsn(DUP)
-        mn.visitInsn(FCONST_1)
-        mn.visitMethodInsn(INVOKESPECIAL, "org/joml/Vector4f", "<init>", "(F)V", false)
+        run {
+            // array[1]
+            mn.visitInsn(ICONST_1)
+            // New instance
+            mn.visitTypeInsn(NEW, "com/primogemstudio/advancedfmk/kui/elements/TextElement")
+            mn.visitInsn(DUP)
+            // arg 1
+            mn.visitLdcInsn("test")
+            // arg 2
+            mn.visitTypeInsn(NEW, "org/joml/Vector2f")
+            mn.visitInsn(DUP)
+            mn.visitInsn(FCONST_0)
+            mn.visitInsn(FCONST_0)
+            mn.visitMethodInsn(INVOKESPECIAL, "org/joml/Vector2f", "<init>", "(FF)V", false)
+            // arg3
+            mn.visitLdcInsn("测试！Hello world from UI compositor!")
+            // arg4
+            mn.visitTypeInsn(NEW, "org/joml/Vector4f")
+            mn.visitInsn(DUP)
+            mn.visitInsn(FCONST_1)
+            mn.visitMethodInsn(INVOKESPECIAL, "org/joml/Vector4f", "<init>", "(F)V", false)
+            // arg 5
+            mn.visitLdcInsn(9)
+            // arg 6
+            mn.visitLdcInsn(false)
 
-        mn.visitIntInsn(BIPUSH, 9)
-        mn.visitInsn(ICONST_0)
-        mn.visitIntInsn(BIPUSH, 32)
-        mn.visitInsn(ACONST_NULL)
 
-        mn.visitMethodInsn(INVOKESPECIAL, "com/primogemstudio/advancedfmk/kui/elements/TextElement", "<init>", "(Ljava/lang/String;Lorg/joml/Vector2f;Ljava/lang/String;Lorg/joml/Vector4f;IZILkotlin/jvm/internal/DefaultConstructorMarker;)V", false)
-        mn.visitInsn(AASTORE)
-
-        mn.visitVarInsn(ALOAD, 1)
+            mn.visitMethodInsn(INVOKESPECIAL, "com/primogemstudio/advancedfmk/kui/elements/TextElement", "<init>", "(Ljava/lang/String;Lorg/joml/Vector2f;Ljava/lang/String;Lorg/joml/Vector4f;IZ)V", false)
+            mn.visitInsn(AASTORE)
+            mn.visitVarInsn(ALOAD, 1)
+        }
+        // build list
         mn.visitMethodInsn(INVOKESTATIC, "kotlin/collections/CollectionsKt", "listOf", "([Ljava/lang/Object;)Ljava/util/List;", false)
+        // final init
         mn.visitMethodInsn(INVOKESPECIAL, "com/primogemstudio/advancedfmk/kui/elements/GroupElement", "<init>", "(Ljava/lang/String;Ljava/util/List;)V", false)
-
+        // save to variable
         mn.visitFieldInsn(PUTFIELD, root.className.replace(".", "/"), "internal", "Lcom/primogemstudio/advancedfmk/kui/elements/UIElement;")
 
         mn.visitInsn(RETURN)
