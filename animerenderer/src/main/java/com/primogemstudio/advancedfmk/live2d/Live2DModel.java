1b3c5df502e5cf3367fe1cb33d4af1b7e9780f11
Jack253-png <guoxiuchen20170402@163.com>
2024-08-25 10:35:18 +0800

    Disable cleaner, bump to 1.1.4

--- animerenderer/src/main/java/com/primogemstudio/advancedfmk/live2d/Live2DModel.java
+++ animerenderer/src/main/java/com/primogemstudio/advancedfmk/live2d/Live2DModel.java
@@ -2,6 +2,7 @@ package com.primogemstudio.advancedfmk.live2d;
 
 import com.primogemstudio.advancedfmk.interfaces.AccessFromNative;
 
+import java.io.File;
 import java.lang.ref.Cleaner;
 
 public class Live2DModel implements AutoCloseable {
@@ -15,9 +16,9 @@ public class Live2DModel implements AutoCloseable {
     private final Cleaner.Cleanable cleaner;
 
     public Live2DModel(String name, String path) {
+        if (!new File(path + name + ".model3.json").exists()) throw new IllegalStateException("File not found with name=" + name + ", path=" + path);
         load(name, path);
-        var _ptr = ptr;
-        cleaner = Live2DNative.cleaner.register(this, () -> release(_ptr));
+        cleaner = Live2DNative.cleaner.register(this, () -> {});
     }
 
     @Override
